927ab6cfba6f8c71014b887399996555718d5a6f
Sam Hon <coffeedaemon@gmail.com>
2017-11-29 19:07:23 +0800

    Automated commit at 2017-11-29 11:07:23 UTC by middleman-deploy 2.0.0-alpha

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,27 +1,90 @@
-$(document).ready(function() {
-  // bindScrollSpy();
-  // bindNavBarMenu();
-});
-
-var lastScrollTop = 0;
-
-function bindScrollSpy() {
-  $(window).scroll($.debounce(30, function(event){
-     var st = $(this).scrollTop();
-     if (st > lastScrollTop){
-       // downscroll code
-       $('.navbar').addClass('minibar')
-     } else {
-      // upscroll code
-      $('.navbar').removeClass('minibar')
-     }
-     lastScrollTop = st;
-  }));
-}
-
-function bindNavBarMenu() {
-  $('.navbar').on('show.bs.collapse hide.bs.collapse' , function () {
-    $('.navbar').removeClass('minibar')
+(function() {
+  $(document).ready(function() {
+    initHeadroom();
+    bindContactFormSubmitButton();
   });
-}
-;
+
+  function initHeadroom() {
+    var navbar = document.querySelector(".navbar");
+    var headroom  = new Headroom(navbar);
+    headroom.init();
+  }
+
+  function formValid() {
+    return $('form:valid').length > 0;
+  }
+
+  function resetForm() {
+    $('form')[0].reset();
+  }
+
+  function serializeFormData() {
+    return $('form').serializeArray().reduce(function(a, x) { a[x.name] = x.value; return a; }, {});
+  }
+
+  function hideForm() {
+    $('.contactModal').modal('hide');
+  }
+
+  function showForm() {
+    $('.contactModal').modal('show');
+  }
+
+  function formSuccess() {
+    swal({
+      type: 'success',
+      title: 'Inquiry Sent!',
+      showConfirmButton: true,
+      timer: 2500,
+      text: 'Thank you for sending in your inquiry, I will get back to you as soon as possible.',
+    });
+    unanimateSubmitButton();
+    hideForm();
+    resetForm();
+  }
+
+  function formError() {
+    swal({
+      type: 'error',
+      title: 'Ops!',
+      showConfirmButton: true,
+      timer: 5000,
+      html: 'We are having trouble receiving your inquiry.<br>Please try again later.',
+    });
+    unanimateSubmitButton();
+  }
+
+  function formPerformAjax(formData) {
+    $.ajax({
+      url: "https://formspree.io/hello@gangtv.com.sg",
+      method: "POST",
+      data: formData,
+      dataType: "json",
+      timeout: 5000,
+    }).done(function() {
+      formSuccess();
+    }).fail(function() {
+      formError();
+    });
+  }
+
+  function animateSubmitButton(){
+    $('form.contact-form input[type="submit"]').addClass('progress-bar progress-bar-striped progress-bar-animated');
+  }
+
+  function unanimateSubmitButton() {
+    $('form.contact-form input[type="submit"]').removeClass('progress-bar progress-bar-striped progress-bar-animated');
+  }
+
+  function bindContactFormSubmitButton() {
+    $('form.contact-form input[type="submit"]').on('click', function(e) {
+      if(formValid()) {
+        e.preventDefault();
+        animateSubmitButton();
+        var data = serializeFormData();
+        formPerformAjax(data);
+      }
+    });
+  }
+
+})();
